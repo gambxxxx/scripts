239a6daed01a504c914abc5bdd924b43941f3900
gambxxxx <34711706+gambxxxx@users.noreply.github.com>
2018-04-16 22:00:24 +0200

    Add files via upload

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,45 +1,10 @@
 #!/bin/bash
 
-### Generate ssl certificate script
-
-
-
-#Required
-domain=$1
-commonname=$domain
- 
-#Change to your company details
-country=MK
-locality=Macedonia
-organization=gorgievski
-organizationalunit=IT
-email=gorgievskip@gmail.com
- 
-#Optional
-echo "Write password"
-read password
- 
-#Generate a key
-openssl genrsa -out -passout pass:$password -out $domain.key 2048 -noout
- 
-#Remove passphrase from the key. Comment the line out to keep the passphrase
-#echo "Removing passphrase from key"
-#openssl rsa -in $domain.key -passin pass:$password -out $domain.key
- 
-#Create the request
-echo "Creating CSR"
-openssl req -new -key $domain.key -out $domain.csr -passin pass:$password \
-    -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
- 
-echo "---------------------------"
-echo "-----Below is your CSR-----"
-echo "---------------------------"
-echo
-cat $domain.csr
- 
-echo
-echo "---------------------------"
-echo "-----Below is your Key-----"
-echo "---------------------------"
-echo
-cat $domain.key
+#   Generate Key
+openssl req -new -x509 -days 365 -sha1 -newkey rsa:1024 \
+-nodes -keyout server.key -out server.crt \
+-subj '/O=Company/OU=Department/CN=www.example.com'
+#   Generate Certificate
+openssl req -new -sha1 -newkey rsa:1024 -nodes \ 
+-keyout server.key -out www.example.com.csr \ 
+-subj '/O=Company/OU=Department/CN=www.example.com'
