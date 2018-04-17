a8c0da381a548eb26e770264e8fe5c39dba8828f
Petar Gjorgievski <perozawow@gmail.com>
2018-04-17 16:55:37 +0200

    TicTacToe building blocks

---
+++ practice-stuff/python-stuff/Tic_Tac_Toe.py
@@ -0,0 +1,41 @@
+def display_board(board):
+    pass
+
+def player_input():
+    pass
+
+def place_marker(board, marker, position):
+    pass
+
+def win_check(board, mark):
+    pass
+
+def choose_first():
+    pass
+
+def space_check(board, position):
+    pass
+
+def full_board_check (board):
+    pass
+
+def player_choic(board):
+    pass
+
+def replay():
+    pass
+
+print('Welcome to Tic tac Toe!')
+# while True:
+#   Set the game up here
+#   pass
+
+#   while game_on:
+#   player1's turn
+
+
+#   player2's turn.
+#       pass
+
+#   if not reply():
+#       break
