54f897cfe236bf69260a627883208b77d22122b9
Petar Gjorgievski <perozawow@gmail.com>
2018-04-17 10:13:34 +0200

    added parameters to generate_cert script

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-
+# Run with SUDO
+echo "Enter directory to generate key and cert"
+read $1
+echo "Enter directory to save key and cert"
+read $2
+cd $HOME
+mkdir $1
+mkdir -p $2
 #   Generate Key
 openssl req -new -x509 -days 365 -sha1 -newkey rsa:1024 \
 -nodes -keyout server.key -out server.crt \
--subj '/O=Company/OU=Department/CN=www.example.com'
+-subj '/O=Company/OU=Department/CN=127.0.0.1'
 #   Generate Certificate
 openssl req -new -sha1 -newkey rsa:1024 -nodes \ 
 -keyout server.key -out www.example.com.csr \ 
--subj '/O=Company/OU=Department/CN=www.example.com'
+-subj '/O=Company/OU=Department/CN=127.0.0.1'
+
+cd $HOME
+mv $1 $2
