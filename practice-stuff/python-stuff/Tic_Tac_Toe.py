7ebf8c42080e23a1d8e6e6b6992dbc8511815beb
Petar Gjorgievski <perozawow@gmail.com>
2018-04-17 17:29:30 +0200

    added some functions to tictactoe

--- practice-stuff/python-stuff/Tic_Tac_Toe.py
+++ practice-stuff/python-stuff/Tic_Tac_Toe.py
@@ -1,8 +1,23 @@
 def display_board(board):
-    pass
+    
+    print(board[7]+'|'+board[8]+'|'+board[9])
+    print('-------------------------------')
+    print(board[4]+'|'+board[5]+'|'+board[6])
+    print('-------------------------------')
+    print(board[1]+'|'+board[2]+'|'+board[3])
 
 def player_input():
-    pass
+    marker =''
+    #Keep asking to choose X or O
+    #assign vtoriot na sprotivnoto
+    while marker != 'X' and marker != 'O':
+        marker=input('Player1 choose X or O')
+    player1=marker
+    if player1 == 'X':
+        player2 =='O'
+    else:
+        player1=='O'
+        player2=='X'
 
 def place_marker(board, marker, position):
     pass
@@ -19,13 +34,16 @@ def space_check(board, position):
 def full_board_check (board):
     pass
 
-def player_choic(board):
+def player_choice(board):
     pass
 
 def replay():
     pass
 
 print('Welcome to Tic tac Toe!')
+
+test_board=['#','X','O','X','O','X','X','O','X','O','X']
+display_board(test_board)
 # while True:
 #   Set the game up here
 #   pass
