23fede2f46e4a6fdfdd852a00961dc1a0e8485fa
Petar Gjorgievski <perozawow@gmail.com>
2018-04-19 10:36:09 +0200

    some fix

--- practice-stuff/python-stuff/python_site_checker.py
+++ practice-stuff/python-stuff/python_site_checker.py
@@ -3,16 +3,19 @@ import time
 from colorama import Fore
 from colorama import Style
 
-
 def check_uptime():
     while True:
-        
-        if print(urllib.request.urlopen("http://allutomotive.com/").getcode()) == '200':
-            print (f'Site is {Fore.RED}DOWN{Style.RESET_ALL}')
-            break
-        else:
+        try:
+            print(urllib.request.urlopen("http://localhost:7070/").getcode())
             print (f'Site is {Fore.GREEN}UP{Style.RESET_ALL}')
-            print (urllib.request.urlopen("http://allutomotive.com/").getcode())
+        except:
+            print (f'Site is {Fore.RED}DOWN{Style.RESET_ALL}')
+        #if print(urllib.request.urlopen("http://localhost:7070/").getcode()) == '200':
+           # print (f'Site is {Fore.RED}DOWN{Style.RESET_ALL}')
+            #break
+        finally:
+            #print (f'Site is {Fore.GREEN}UP{Style.RESET_ALL}')
+            #print (urllib.request.urlopen("http://localhost:7070/").getcode())
             time.sleep(5)
 
 check_uptime()
