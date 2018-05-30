2ac6eba634c637519ce8ddac1e5392a6360fd069
Petar Gjorgievski <perozawow@gmail.com>
2018-05-30 17:19:44 +0200

    mda

---
+++ useful/useful-installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+##
+##
+sudo apt-get update
+sudo apt-get install atom
+sudo apt-get install vscode
+sudo apt-get install jenkins
+sudo apt-get install apache2
+sudo apt-get install nginx
